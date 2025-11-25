3041264feaa610519dbc66620b13e98e7efa374f
Thomas Pinetz <thomas.pinetz@meduniwien.ac.at>
2025-11-25 13:16:02 +0100

    updated constants.

---
+++ models/constants.py
@@ -0,0 +1,26 @@
+IMAGE="image"
+MASK="mask"
+TARGET="target"
+SOURCE="source"
+PAT_ID="pat_id"
+NIMAGE="noisy"
+SIGMA = "sigma"
+
+INIT_ZERO = "zero"
+INIT_NOISY = "noisy"
+
+NOISE_MULTI = "multiplicative"
+NOISE_EDM = "edm"
+NOISE_ADD = "additive"
+NOISE_COMB = "combined"
+
+MODEL_UNETPLUSPLUS = "unetplusplus"
+MODEL_UNET = "unet"
+MODEL_ENERGY = "energy"
+SONG_UNET = "song"
+SONG_3DUNET = "song3d"
+TRANS_UNET = "transunet"
+
+
+STUDY_SUDETES = "SUDETES"
+STUDY_APENNINES = "APENNINES"
